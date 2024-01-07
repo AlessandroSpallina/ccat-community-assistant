01b1a2088ed45bf85e24eb6dbe496f227739328b
AlessandroSpallina <alessandrospallina1@gmail.com>
2024-01-07 04:02:27 +0100

    Better meetup scraping

--- communityassistant.py
+++ communityassistant.py
@@ -5,6 +5,9 @@ from typing import List
 from .meetup import Meetup
 
 
+m = Meetup('python-torino')
+
+
 class PluginSettings(BaseModel):
     assistant_scope: str = Field(
         default="""Act as the Community Assistant of 'Python Torino', an organization focused on python and organizing public events with talks with cool people.
@@ -39,34 +42,47 @@ You help people connect with the organization, entice them to attend events, and
         description="Check the meetup.com link, for example https://www.meetup.com/python-torino -> python-torino"
     )
 
+    procedural_k: int = Field(
+        default="1",
+        ge=0,
+        title="Procedural K",
+        description="Number of tools to recall from procedural memory after an user message"
+    )
+
+    procedural_threshold: float = Field(
+        default="0.83",
+        ge=0,
+        le=1,
+        title="Procedural Threshold",
+        description="Threshold for tools to recall from procedural memory after an user message"
+    )
+
 
 @plugin
 def settings_model():
     return PluginSettings
 
 
-@tool
-def event_details(tool_input, cat):
-    """When user asks for an event of the Community. Input is always None
-Examples of questions you might face:
-- che mi sai dire dell'evento?
-- dettagli evento?
-- dimmi di più"""
+# @tool
+# def event_details(tool_input, cat):
+#     """When user asks for an event of the Community. Input is always None
+# Examples of questions you might face:
+# - che mi sai dire dell'evento?
+# - dettagli evento?
+# - dimmi di più"""
 
-    return "BLABLABLABLA"
+#     m.get_event_detail()
+#     return "BLABLABLABLA"
 
 
 @hook
 def agent_prompt_prefix(prefix, cat):
-    prefix = f"{cat.mad_hatter.get_plugin().load_settings()['assistant_scope']} You answer Human with a focus on the following context. ALWAYS answer in the same language the human is talking to you!"
+    prefix = f"{cat.mad_hatter.get_plugin().load_settings()['assistant_scope']} You answer Human with a focus on the following context. NEVER answer questions not in topic with the context. ALWAYS answer in the same language the human is talking to you!"
     return prefix
-
 
 
 @hook
 def agent_prompt_suffix(prompt_suffix, cat):
-    m = Meetup('python-torino')
-
     prompt_suffix = f""" 
 # Context
 
@@ -82,13 +98,13 @@ DON'T suggest any other contact that isn't present in the following text!
 ## Past events organized by the Community
 Keep in mind that now is {datetime.now().strftime("%A %d %B %Y, %H:%M:%S %Z%z")}, so evaluate time and timezone and don't be fooled with dates!
 ```text
-{m.get_past_events()}
+{m.past_events}
 ```
 
 ## Upcoming events organized by the Community
 Keep in mind that now is {datetime.now().strftime("%A %d %B %Y, %H:%M:%S %Z%z")} and such events stay for around 2 hours, so evaluate time and timezone to guess if the event is ongoing just now!
 ```text
-{m.get_upcoming_events()}
+{m.upcoming_events}
 ```
 
 {{episodic_memory}}
@@ -106,6 +122,7 @@ Keep in mind that now is {datetime.now().strftime("%A %d %B %Y, %H:%M:%S %Z%z")}
 
 @hook
 def before_cat_recalls_procedural_memories(default_procedural_recall_config, cat):
-    default_procedural_recall_config["k"] = 1
+    default_procedural_recall_config["k"] = cat.mad_hatter.get_plugin().load_settings()['procedural_k']
+    default_procedural_recall_config["threshold"] = cat.mad_hatter.get_plugin().load_settings()['procedural_threshold']
 
     return default_procedural_recall_config
--- meetup.py
+++ meetup.py
@@ -1,34 +1,45 @@
 import httpx
-import schedule
-import time
 from typing import List
 from pydantic import BaseModel
 from bs4 import BeautifulSoup
-from cat.mad_hatter.decorators import hook
+from cat.log import log
+import time
+
+
+CACHE_LIFETIME = 60 # seconds
 
 
 class Event(BaseModel):
     name: str
-    time: str # or python datetime
+    time: str
     link: str
     location: str = 'N/A'
     details: str = 'N/A' # would be nice to automatically ingest linked pages
     slide_links: List[str] = [] # obtained from https://speakerdeck.com/pythontorino
 
     def __repr__(self) -> str:
-        return f'Event: `{self.name}`, time:{self.time}, link:`{self.link}`, location:`{self.location}, details:`{self.details}`, slide_links:`{self.slide_links}'
+        # I'm not sure this is the best way to format event info
+        return f'Event `{self.name}`\n- time: `{self.time}`\n- link: `{self.link}`\n- location: `{self.location}\n'
+
 
 class Meetup:
     def __init__(self, organization_name):
         self._organization_name = organization_name
         self._past_events_url = f'https://www.meetup.com/{organization_name}/events/?type=past'
         self._upcoming_events_url = f'https://www.meetup.com/{organization_name}/events/?type=upcoming'
+
+        self._past_events_timestamp = -1 # filled with the timestamp of the last update
+        self._past_events = []
+        self._upcoming_events_timestamp = -1 # filled with the timestamp of the last update
+        self._upcoming_events = []
     
     def _request(self, url: str, method='GET') -> list[Event]:
         r = httpx.request(method, url)
         r.raise_for_status()
-
-        soup = BeautifulSoup(r.text, 'html.parser')
+        return r.text
+    
+    def _scrape_events(self, text: str):
+        soup = BeautifulSoup(text, 'html.parser')
         
         event_divs = soup.select('div[id^="e-"], div[id^="ep-"]')
 
@@ -38,15 +49,43 @@ class Meetup:
             time = event_div.find('time').text.strip()
             title = event_div.find('span', class_='ds-font-title-3').text.strip()
             link = event_div.find('a')['href']
+            location = event_div.find('div', class_='flex items-start space-x-1.5').find('span', class_='text-gray6').text.strip()
+            # location = event_div.find_all('span', class_='text-gray6')[-1].text.strip()
             events.append(Event(
                 name=title,
                 time=time,
-                link=link
+                link=link,
+                location = location if 'event has passed' not in location else 'N/A'
             ))
         return events
+    
+    def _scrape_event(self, text: str): # TODO
+        print("s")
+        print("")
 
-    def get_past_events(self):
-        return self._request(self._past_events_url)
+    def _get_past_events(self): # use this to get past_events doing an GET request on meetup.com
+        return self._scrape_events(self._request(self._past_events_url))
 
-    def get_upcoming_events(self):
-        return self._request(self._upcoming_events_url)
+    def _get_upcoming_events(self): # use this to get upcoming_events doing an GET request on meetup.com
+        return self._scrape_events(self._request(self._upcoming_events_url))
+    
+    def get_event_detail(self, event_url: str): # TODO
+        return self._scrape_event(self._request(event_url))
+    
+    @property    
+    def past_events(self): # use this to get past_events using a cached result
+        current_timestamp = time.time()
+        if current_timestamp - self._past_events_timestamp >= CACHE_LIFETIME:
+            self._past_events_timestamp = current_timestamp
+            self._past_events = self._get_past_events()
+            log.info(f"Community Manager scraped {len(self._past_events)} past events")
+        return self._past_events
+
+    @property    
+    def upcoming_events(self): # use this to get upcoming_events using a cached result
+        current_timestamp = time.time()
+        if current_timestamp - self._upcoming_events_timestamp >= CACHE_LIFETIME:
+            self._upcoming_events_timestamp = current_timestamp
+            self._upcoming_events = self._get_upcoming_events()
+            log.info(f"Community Manager scraped {len(self._upcoming_events)} upcoming events")
+        return self._upcoming_events
